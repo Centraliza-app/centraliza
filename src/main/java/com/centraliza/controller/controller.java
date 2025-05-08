2c339d58c3bcc14722bb74e1a06903bb08e7a8cf
gabriel <biel_fahl@hotmail.com>
2025-05-08 20:57:10 -0300

    first commit

---
+++ src/main/java/com/centraliza/controller/controller.java
@@ -0,0 +1,13 @@
+package com.centraliza.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class controller {
+    @GetMapping("/")
+    public String tarefa(){
+        return "test";
+    }
+}
