faccf05852f6d94d8ac8bfa3d5393957aaa74aa7
Brafero <139059957+Brafero@users.noreply.github.com>
2025-05-20 21:11:02 -0300

    Update TarefaService.js
    Especificar como usar o Front

--- frontend/src/services/TarefaService.js
+++ frontend/src/services/TarefaService.js
@@ -4,7 +4,7 @@ const API_URL = 'http://localhost:8080/tarefas';
 
 // Altere isso conforme a senha que apareceu no terminal
 const username = 'user';
-const password = 'f8397bc4-9b7a-449d-890d-efdcc0baf623';
+const password = 'f8397bc4-9b7a-449d-890d-efdcc0baf623'; //Ao iniciar a aplicação, é preciso colocar a senha gerada pelo backend aqui ***Vou mudar isso***
 
 const auth = {
   auth: {
