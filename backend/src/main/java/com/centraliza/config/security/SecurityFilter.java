9443f3fc2f4f158f0c6d2ad59c2d072404687566
Ygor Gonçalves <ysgoncalves@gmail.com>
2025-08-31 17:10:58 -0300

    Adicionar anotações @NonNull aos parâmetros doFilterInternal
    Adicionadas as anotações @NonNull do Lombok aos parâmetros HttpServletRequest, HttpServletResponse e FilterChain em doFilterInternal para melhorar a segurança nula e esclarecer o contrato do método.

--- backend/src/main/java/com/centraliza/config/security/SecurityFilter.java
+++ backend/src/main/java/com/centraliza/config/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,6 +26,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     private UserDetailsService userDetailsService;
 
     @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
 
         if (token != null) {
